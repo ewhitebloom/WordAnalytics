b4682d96f224a705c6630f9648793bc511356f8e
Eric Kelly <heroiceric@gmail.com>
2014-03-17 14:58:27 -0400

    Refactor formatting
    The most important thing to keep in mind when you’re writing code,
    other than if it works, is whether not it can be easily read and
    understood.
    
    Some general guidelines are to keep lines less than 80 characters long
    and to break your code into multiple lines if you ever find yourself
    needing a semicolon (`;`) or go past 80 characters.
    
    There's a good ruby style guide at
    https://github.com/bbatsov/ruby-style-guide

--- lib/wordanalysis.rb
+++ lib/wordanalysis.rb
@@ -8,21 +8,45 @@ class WordAnalysis
   end
 
   def count(word_letter_or_symbol)
-    if word_letter_or_symbol.downcase == 'word'; items = @words; elsif word_letter_or_symbol.downcase == 'letter'; items = @letters; elsif word_letter_or_symbol.downcase == 'symbol'; items = @symbols; else; puts 'Please ask for word, letter or symbol count.'; exit; end;
+    if word_letter_or_symbol.downcase == 'word'
+      items = @words
+    elsif word_letter_or_symbol.downcase == 'letter'
+      items = @letters
+    elsif word_letter_or_symbol.downcase == 'symbol'
+      items = @symbols
+    else
+      puts 'Please ask for word, letter or symbol count.'
+      exit
+    end
+
     counts = {}
-    items.each { |item| item.downcase!; if counts.has_key?(item); counts[item] += 1; else counts[item] = 1; end; }
+
+    items.each do |item|
+      item.downcase!
+
+      if counts.has_key?(item)
+        counts[item] += 1
+      else
+        counts[item] = 1
+      end
+    end
+
     counts.sort_by{ |k,v| v }.reverse
   end
 
   def once(counts)
     counter = 0
-    counts.each{ |pair| if pair[1] == 1; counter += 1; end; }
+
+    counts.each do |pair|
+      counter += 1 if pair[1] == 1
+    end
+
     counter
   end
 
   def common_items(counts)
     top = []
-    counts[0..2].each{ |pair| top << pair[0]; }
+    counts[0..2].each{ |pair| top << pair[0] }
     top.join("\t")
   end
 end
